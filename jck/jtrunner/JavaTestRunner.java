8634ef94b7d7d5177a6ed9916af8d73b92914bf4
Mesbah-Alam <Mesbah_Alam@ca.ibm.com>
2021-09-22 11:47:28 -0400

    Backport change made to STF impl: RMID process
    Signed-off-by: Mesbah-Alam <Mesbah_Alam@ca.ibm.com>

--- jck/jtrunner/JavaTestRunner.java
+++ jck/jtrunner/JavaTestRunner.java
@@ -807,7 +807,8 @@ public class JavaTestRunner {
 				javatestAgent = startSubProcess("javatestAgent",javatestAgentCmd);
 
 				// We only need RMI registry and RMI activation daemon processes for tests under api/java_rmi
-				if (tests.contains("api/java_rmi")) {
+				if (tests.contains("api/java_rmi") && 
+					(jckVersion.contains("jck8") || jckVersion.contains("jck11") || jckVersion.contains("jck16"))) {
 					String pathToRmiRegistry = testJdk + File.separator + "bin" + File.separator + "rmiregistry";
 					List<String> rmiRegistryCmd = new ArrayList<>();
 					rmiRegistryCmd.add(pathToRmiRegistry);
